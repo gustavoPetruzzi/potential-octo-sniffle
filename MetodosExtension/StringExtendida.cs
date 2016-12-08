e618a6096d6348b24cbbe3d7d7c37dec24857908
gustavo <gustavo@debian.gustavo.example.com>
2016-12-08 01:27:44 -0300

    Eventos

---
+++ MetodosExtension/StringExtendida.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosExtension
+{
+    public static class StringExtendida
+    {
+        public static int CantidadPalabras(this string estring)
+        {
+            string[] palabras;
+            palabras = estring.Split(',', ' ');
+            return palabras.Length;
+        }
+
+    }
+}
